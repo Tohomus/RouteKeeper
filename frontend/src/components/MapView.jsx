2c4eed632d09fd3c017bce56741c8f24dfb71c37
Thomas <inbox.akanksha2@gmail.com>
2026-01-04 23:19:30 +0530

    Fix Leaflet marker icon issue

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,22 +1,33 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+/* FIX LEAFLET MARKER ICON ISSUE */
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 export default function MapView({ latitude, longitude, children }) {
-  if (latitude == null || longitude == null) {
-    return (
-      <div className="h-[400px] w-full flex items-center justify-center text-gray-500">
-        Location not set for in-app map
-      </div>
-    );
-  }
-
   return (
     <MapContainer
       center={[latitude, longitude]}
       zoom={14}
       className="h-[400px] w-full rounded-xl"
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
       <Marker position={[latitude, longitude]} />
+
       {children}
     </MapContainer>
   );
