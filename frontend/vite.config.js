aaa7b7fe05296f0c7818b38d4d4aa0b0095ff11e
Thomas <inbox.akanksha2@gmail.com>
2026-01-07 00:22:10 +0530

    converted to PWA by giving icons inorder get an app like feeling when added to homescreen

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        id:"/",
+        name: "RouteKeeper",
+        short_name: "RouteKeeper",
+        description: "Save and navigate your important routes",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#F8FAFC",
+        theme_color: "#3B82F6",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+        
+      }
+    })
+  ]
+});
